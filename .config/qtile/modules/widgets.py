563083493cbefd20823f7c41a2021d3a888e2202
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2022-09-29 15:06:39 +0200

    added qtile

---
+++ .config/qtile/modules/widgets.py
@@ -0,0 +1,9 @@
+from libqtile import widget
+from libqtile import qtile
+
+widget_defaults = dict(
+    font="sans",
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
